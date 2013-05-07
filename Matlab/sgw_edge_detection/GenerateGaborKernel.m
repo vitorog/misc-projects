0e654fa08a5c3d953182d5acf97483319d684b23
Vitor Gomes <vitorgomes@vitorgomes-desktop.(none)>
2013-05-07 17:46:52 -0300

    -Fixed a lot of errors. The code seems to be working properly now.

--- Matlab/sgw_edge_detection/GenerateGaborKernel.m
+++ Matlab/sgw_edge_detection/GenerateGaborKernel.m
@@ -3,7 +3,7 @@ S = zeros(size,size);
 half_size = round(size/2);
 for x=-half_size:half_size
     for y=-half_size:half_size
-        S(x+(half_size+1),y+(half_size+1)) = exp((-(x^2 + y^2))/2*(sigma^2))*sin((w*((x*cos(theta)) + (y*sin(theta)))));
+        S(x+(half_size+1),y+(half_size+1)) = exp(-(x^2 + y^2)/(2*(sigma^2))) * sin(w*(x*cos(theta) + y*sin(theta)));
     end
 end
 end
